91a3fc3b9b1defa3f226ab60176729fd696678d3
Pedro Gutierrez Jr <pedrogutie0@gmail.com>
2023-08-30 11:40:22 -0500

    first commit, created projects with basic view controllers and Tab Bar controller to navigate between them

---
+++ Controllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  Netflix Clone
+//
+//  Created by Pedro Gutierrez on 8/30/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+
+}
---
+++ Controllers/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Pedro Gutierrez on 8/30/23.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+
+}
